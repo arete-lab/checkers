ee85bd4d6dac8345befe062d36d422d9e6d61c47
Francesco De Falco <defalco.fnc@gmail.com>
2023-05-07 18:44:48 +0200

    Added Full Game file

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,5 +8,8 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrSample           = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1101, "black address is invalid: %s")
+	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1102, "red address is invalid: %s")
+	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1103, "game cannot be parsed")
 )

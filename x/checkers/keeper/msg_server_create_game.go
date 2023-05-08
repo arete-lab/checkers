f590253f5a55ec438df3a4d4d887a8b37e162936
Francesco De Falco <defalco.fnc@gmail.com>
2023-05-08 19:33:42 +0200

    Created and Saved game properly

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,8 +12,31 @@ import (
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("system info not found")
+	}
 
-	return &types.MsgCreateGameResponse{}, nil
+	newIndex := strconv.FormatUint(systemInfo.NextId, 10)
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Board: newGame.String(),
+		Turn:  rules.PieceStrings[newGame.Turn],
+		Black: msg.Black,
+		Red:   msg.Red,
+	}
+
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgCreateGameResponse{
+		GameIndex: newIndex,
+	}, nil
 }
